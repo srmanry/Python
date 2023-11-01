3545ba36ff8dd85d120a42d326711948260b66d7
sr_sumanroy <123sumango@gmail.com>
2023-11-01 08:55:43 -0700

    Operator

--- day_2.py
+++ day_2.py
@@ -1,12 +1,12 @@
-print("program - ----- 2 Multiple datatype input ")
+print("program - ----- 1 & 2 Multiple datatype input **************************** ")
 
 
 name = input("Input your name : ")
 print(name),
-print("My name is " + name),
+print("Name " + name),
 
 age = int(input("Input Age : "))
-print("My age ",+ age)
+print(" Age ",+ age)
 address = input("Input Address : ")
 print("My name is " + name + "\n My home " + address + " My Age ",+age)
 
---
+++ operator_day_3.py
@@ -0,0 +1,102 @@
+
+
+# ArithMatic Operator ****************************  
+"""print("ArithMatic Operator ****************************")
+number1 = int(input("Enter first number : "))
+number2 = int(input("Enter second number : "))
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+print(number1%number2)
+print(number1//number2)
+print(number1**2)
+print(2**3)"""
+
+# ArithMatic Operator **************************** 
+"""
+print("ArithMatic Operator **************************** ")
+
+a = 10
+print(a+4)
+a+=4
+print(a) 
+# a =5
+
+a-=4
+print(a) 
+
+a*=4
+print(a) 
+
+a/=4
+print(a) 
+
+a//=4
+print(a) 
+
+a**=4
+print(a)
+
+# a^=4
+# print(a)
+
+# a>>=4
+print(a)
+
+a<<=4
+print(a)
+
+"""
+
+# Comparison Operators **************************** 
+"""
+print("Comparison Operators **************************** ")
+
+b = 20
+c = 10
+print(b==c)
+print(b!=c)
+print(b>c)
+print(b<c)
+print(b<=c)
+print(b>=c)
+"""
+
+
+# Logical Operators **************************** 
+
+"""print("Logical Operators **************************** ")
+aa = 10
+bb = 20
+print(aa<bb and aa<10)
+print(aa<bb or aa<30)
+print(not(aa<bb or aa<30))"""
+
+
+# Identity Operators **************************** 
+
+'''print("Identity Operators **************************** ")
+aa = 10
+cc =20
+bb = aa
+print(aa is bb)
+print(aa is not bb)
+print(aa is cc)
+print("name" is "name")
+print("name" is not "name")'''
+
+# Membership Operators **************************** 
+
+print("Membership Operators **************************** ")
+
+x = ["apple", "banana"]
+print("banana" in x)
+print("aaaa" in x)
+print("apple" not in x)
+print("appll" not in x)
+
+
+
+
+
