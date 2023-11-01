ba8dc8e1e29a92e611ed4decab4b96c0940cd638
sr_sumanroy <123sumango@gmail.com>
2023-11-01 11:53:06 -0700

    Python list

---
+++ list.py
@@ -0,0 +1,28 @@
+numberlist = [1,2,3,4,5,6,7,8,9]
+print(numberlist)
+print(len(numberlist))
+print(type(numberlist))
+print(numberlist[0])
+print(numberlist[-1])
+print(numberlist[+1])
+print(numberlist[5])
+
+
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
+
+namelist = list(("A","B","C","D"))
+print(namelist)
+print(namelist[-1])
+print(namelist[+1])
+print(namelist [1:])
+print("a" is namelist)
+print("A" is namelist)
+print("a" is not namelist)
+print("A" is not namelist)
+print(namelist +["E","F","G","H",7])
+print(f"{namelist} Add the number")
+print(namelist *2)
+# print(namelist +3)
+# print(len(namelist))
+
