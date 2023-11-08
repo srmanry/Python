3b0d41ec0a6b8f0b6e7545927d5ca2dc93a4bcb3
sr_sumanroy <123sumango@gmail.com>
2023-11-07 22:10:15 -0800

    IF else

---
+++ if-else.py
@@ -0,0 +1,37 @@
+
+# Program -------------- 1
+a = 10
+if a>20:
+    print("False")
+    
+else:
+    print("True")
+    
+num1 = 10
+num2 = 0
+num3 = 30
+if num1>num2:
+    num1 > num3
+    print("Num 1")
+# else: print("number1")
+if num2>num1:
+    num2>num3
+    print("Num 2")
+else: print("number3")
+
+
+# Program -------------- 2
+#  Latter marks
+marks = 90
+if marks >=80:
+    print("A+")
+elif marks >=70:
+    print("A")
+elif marks >=60:
+    print("B")
+elif marks >=70:
+    print("C")
+else:
+    print("failed")
+    
+print(marks if marks>50 else 60)
