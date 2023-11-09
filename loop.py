e295b30d282d91f8caaac3ff6147c592d752b590
sr_sumanroy <123sumango@gmail.com>
2023-11-08 23:31:07 -0800

    Loop

---
+++ loop.py
@@ -0,0 +1,16 @@
+# i = 20
+# # Even Number program
+# while i<=100:
+#     i =i+2
+#     print(f"Even number =  {i}")
+
+
+# Odd Number print program
+sum = 0
+i =10
+while i<=100:
+    sum =sum +i
+    i= i +1
+print(sum) 
+    
+
