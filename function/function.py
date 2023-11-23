54938cfe557ee3db569d9ccf400693f1fd42e6e0
sr_sumanroy <123sumango@gmail.com>
2023-11-22 23:43:39 -0800

    python

--- function/function.py
+++ function/function.py
@@ -2,7 +2,12 @@ def firstFuncton():
     print("Print First Function")
 firstFuncton()
 
+#  python  function
 def sumfunction(a,b):
     sumfunction = a+b
     print(sumfunction)
 sumfunction(50,60)
+def functiontow(c,d):
+    functiontow =c*d
+    print(functiontow),
+functiontow(10,20)
