2a02481bd104c1b781dc07fb4b2e1eb4d281fe2f
sr_sumanroy <sumantachtg@gmail.com>
2023-11-25 12:59:06 +0600

    python

--- typecast.py
+++ typecast.py
@@ -3,11 +3,12 @@
 
 name = input("Enter your name : ")
 
-# roll = int(input("Enter your roll : ")) 
-roll =20
+roll = int(input("Enter your roll : ")) 
+clas = input("Input your class :")
+address = input("Input your address : ")
 
+print("Name :" +name + "\nRoll " + str(roll)+"\nClass :"+clas +"\nAddress :"+ address)
 
-print("Hello Im  " +name + " My roll " + str(roll))
+# print(f" 2+2 = {2+2}")
+# print(id(roll)),
 
-print(f" 2+2 = {2+2}")
-print(id(roll))
