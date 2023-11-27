8096c6dd85b9ef4c30885b966418f24f9f05112c
sr_sumanroy <sumantachtg@gmail.com>
2023-11-28 02:23:42 +0600

    function

--- function/function.py
+++ function/function.py
@@ -6,3 +6,30 @@ def sumfunction(a,b):
     sumfunction = a+b
     print(sumfunction)
 sumfunction(50,60)
+
+def functio(name):
+    print("My name : ",name)
+functio("Suman Roy")
+
+def add(c,d):
+    if c>d:
+        print("Large number",c)
+    else:
+       print("Large number",d)
+
+add(60,80)
+# # print(result)
+
+def parameterpass(a,b,c):
+    if a>b:
+        print("big number a = ",a)
+    elif a>c:
+        print(" Large number A = ",a)
+    elif b>c:
+        print("B is Large number =",b)
+    else:
+        print("All false  C large number :",c)
+parameterpass(10,90,30)
+
+        
+    
