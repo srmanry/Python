c5a1acf6986593091ae8c71808627ec6dc6b4b0a
sr_sumanroy <sumantachtg@gmail.com>
2023-11-28 02:49:10 +0600

    python

--- function/function.py
+++ function/function.py
@@ -1,35 +1,48 @@
-def firstFuncton():
-    print("Print First Function")
-firstFuncton()
+# def firstFuncton():
+#     print("Print First Function")
+# firstFuncton()
 
-def sumfunction(a,b):
-    sumfunction = a+b
-    print(sumfunction)
-sumfunction(50,60)
+# def sumfunction(a,b):
+#     sumfunction = a+b
+#     print(sumfunction)
+# sumfunction(50,60)
 
-def functio(name):
-    print("My name : ",name)
-functio("Suman Roy")
+# def functio(name):
+#     print("My name : ",name)
+# functio("Suman Roy")
 
-def add(c,d):
-    if c>d:
-        print("Large number",c)
-    else:
-       print("Large number",d)
+# def add(c,d):
+#     if c>d:
+#         print("Large number",c)
+#     else:
+#        print("Large number",d)
 
-add(60,80)
-# # print(result)
+# add(60,80)
+# # # print(result)
 
-def parameterpass(a,b,c):
-    if a>b:
-        print("big number a = ",a)
-    elif a>c:
-        print(" Large number A = ",a)
-    elif b>c:
-        print("B is Large number =",b)
-    else:
-        print("All false  C large number :",c)
-parameterpass(10,90,30)
+# def parameterpass(a,b,c):
+#     if a>b:
+#         print("big number a = ",a)
+#     elif a>c:
+#         print(" Large number A = ",a)
+#     elif b>c:
+#         print("B is Large number =",b)
+#     else:
+#         print("All false  C large number :",c)
+# parameterpass(10,90,30)
 
-        
+    
+def adision(x,y):
+    a = x+y
+    b =x-y
+    print(a)
+    print(b)
+
+   # return a,b
+adision(10,20)
+# sum,sub= adision(2,5)
+# print("print value of sum = ",sum)
+# print(" print value of sub =", sub)
+
+    
     
